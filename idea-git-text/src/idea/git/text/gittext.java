313e69f2b474d5ba716d5686cf8667c3438c9749
xiaoli <xiaoli@qq.com>
2019-06-17 14:14:04 +0800

    将工程修改后加入到库

--- idea-git-text/src/idea/git/text/gittext.java
+++ idea-git-text/src/idea/git/text/gittext.java
@@ -8,4 +8,7 @@ public class gittext {
         System.out.println("hello bajie");
         System.out.println("hello bajie");
     }
+    public void text(){
+        System.out.println("nihao");
+    }
 }
