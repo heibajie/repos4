9ce6e9db92b17905c91b129e64771f3c5327ba82
xiaoli <xiaoli@qq.com>
2019-06-17 14:09:52 +0800

    将工程加入到库

---
+++ idea-git-text/src/idea/git/text/gittext.java
@@ -0,0 +1,11 @@
+package idea.git.text;
+
+public class gittext {
+    public static void main(String[] args) {
+        System.out.println("hello bajie");
+        System.out.println("hello bajie");
+        System.out.println("hello bajie");
+        System.out.println("hello bajie");
+        System.out.println("hello bajie");
+    }
+}
